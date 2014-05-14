5f4ce2be01d5b7a83ff2a335dae2b036d86944f5
yusukezzz <g0632198@gmail.com>
2014-05-14 14:11:59 +0900

    collect response cookies

--- src/DebugBar/DataCollector/SlimResponseCollector.php
+++ src/DebugBar/DataCollector/SlimResponseCollector.php
@@ -25,6 +25,7 @@ class SlimResponseCollector extends DataCollector implements Renderable
             'content-type' => $this->response->header('Content-Type'),
             'status_code' => $this->response->getStatus(),
             'headers' => $this->getDataFormatter()->formatVar($this->response->headers->all()),
+            'cookies' => $this->getDataFormatter()->formatVar($this->response->cookies->all()),
         ];
     }
 
